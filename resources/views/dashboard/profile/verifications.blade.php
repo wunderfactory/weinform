46cee61665a16ed8a78769727e1f5f4e4386c0dd
Magnus Langanke <magnus.langanke@me.com>
2015-04-10 13:46:13 +0200

    added link to verification

--- resources/views/dashboard/profile/verifications.blade.php
+++ resources/views/dashboard/profile/verifications.blade.php
@@ -61,7 +61,7 @@
                         </div>
                         <div class="col-md-9 right_container">
                             <p class="rose">Eine <strong>Verifikation deiner Identität</strong> ist der einfachste und schnellste Weg, in der wundership Community Vertrauen aufzubauen. Wir gleichen dazu Deine Profildaten mit deinem offiziellen Ausweis ab. Diese Verfizierung ist besonders vertrauenswürdig und sehr wichtig, wenn Du <strong>Sendungen mitnehmen</strong> möchtest.<br> Der Vorgang dauert nur etwa <strong>eine Minute.</strong></p>
-                            <a href="#"><button class="btn btn-default"><strong>Verifikation meiner Identität starten</strong></button></a> 
+                            <a href="{{ action('SettingsVerificationController@getIndex', [Auth::user()->username]) }}"><button class="btn btn-default"><strong>Verifikation meiner Identität starten</strong></button></a>
                           </div>
                     </div>
                 </div> 
