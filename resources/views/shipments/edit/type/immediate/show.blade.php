8fe2bb5c5fa3156aa8b46cf2bb3096be92be22aa
Moritz Ewert <moritz.ewert@wunderfactory.de>
2015-05-06 19:53:16 +0200

    route to x.immediate.destroy correctly

--- resources/views/shipments/edit/type/immediate/show.blade.php
+++ resources/views/shipments/edit/type/immediate/show.blade.php
@@ -2,6 +2,6 @@
     <h3><i class="fa fa-rocket"></i> FastTrack</h3>
     <p class="lead">€ {{ $immediate->price }}</p>
 </div>
-{!! Form::open(['action' => 'post', 'route' => ['shipments.immediate.destroy', $immediate->shipment, $immediate]]) !!}
+{!! Form::open(['method' => 'delete', 'route' => ['shipments.immediate.destroy', $shipment, $immediate]]) !!}
 {!! Form::submit('Ändern', ['class' => 'btn btn-warning btn-block']) !!}
 {!! Form::open() !!}
