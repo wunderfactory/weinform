e19c01afc2b4edc7fbd1ddfba402b709663d903d
Moritz Ewert <moritz.ewert@wunderfactory.de>
2015-05-06 20:11:26 +0200

    make edit page more readable

--- resources/views/shipments/edit.blade.php
+++ resources/views/shipments/edit.blade.php
@@ -26,56 +26,78 @@
 @endsection
 
 @section('content')
-    <div class="container" style="padding-bottom: 40px;">
+    <style>
+        section.shipment-edit {
+            background-color: lightgreen;
+            border-radius: 7px;
+            padding: 10px;
+            margin-bottom: 5px;
+        }
+        section.shipment-edit section {
+            background-color: lightcyan;
+            border-radius: 7px;
+            padding: 3px;
+            margin-bottom: 10px;
+        }
+    </style>
+    <div class="container">
         <h1>Sendung bearbeiten</h1>
-        <h2>Details</h2>
-        <h3>Strecke</h3>
-        <div class="col-sm-6">
-            <h4>Abholung</h4>
-            @if($shipment->origin_id)
-                @include('shipments.edit.origin.show', ['origin' => $shipment->origin])
-                {!! link_to_route('shipments.origin.index', 'Edit', ['shipment' => $shipment], ['class' => 'btn-block btn btn-warning']) !!}
-            @else
-                {!! link_to_route('shipments.origin.index', 'Select', ['shipment' => $shipment], ['class' => 'btn-block btn btn-primary']) !!}
-            @endif
-        </div>
-        <div class="col-sm-6">
-        <h4>Lieferung</h4>
-            @if($shipment->destination_id)
-                @include('shipments.edit.destination.show', ['destination' => $shipment->destination])
-                {!! link_to_route('shipments.destination.index', 'Edit', ['shipment' => $shipment], ['class' => 'btn-block btn btn-warning']) !!}
-            @else
-                {!! link_to_route('shipments.destination.index', 'Select', ['shipment' => $shipment], ['class' => 'btn-block btn btn-primary']) !!}
-            @endif
-        </div>
-        <h3>Größe</h3>
-        @if($shipment->size_id)
-            <p>{{ $shipment->size->name }} <small>{{ $shipment->size->description }}</small></p>
-            {!! link_to_route('shipments.size.index', 'Edit', ['shipment' => $shipment], ['class' => 'btn-block btn btn-warning']) !!}
-        @else
-            {!! link_to_route('shipments.size.index', 'Select', ['shipment' => $shipment], ['class' => 'btn-block btn btn-primary']) !!}
-        @endif
-        <h3>Specs</h3>
-        @if(count($shipment->specs))
-            @foreach($shipment->specs as $spec)
-                <span class="label label-info"><strong>{{ $spec->name }}</strong> {{ $spec->description }}</span>
-            @endforeach
-            {!! link_to_route('shipments.specs.index', 'Edit', ['shipment' => $shipment], ['class' => 'btn-block btn btn-warning']) !!}
-        @else
-            {!! link_to_route('shipments.specs.index', 'Select', ['shipment' => $shipment], ['class' => 'btn-block btn btn-primary']) !!}
-        @endif
-        <h2>Typ</h2>
-        @if($shipment->typeable_id && $shipment->typeable_type)
-            @if($shipment->typeable instanceof Wundership\Auction)
-                @include('shipments.edit.type.auction.show', ['shipment' => $shipment, 'auction' => $shipment->typeable])
-            @elseif($shipment->typeable instanceof Wundership\Immediate)
-                @include('shipments.edit.type.immediate.show', ['shipment' => $shipment, 'immediate' => $shipment->typeable])
-            @endif
-        @else
-            <div class="btn-group btn-group-justified" role="group" aria-label="...">
-                {!! link_to_route('shipments.auction.create', 'Auction', ['shipment' => $shipment], ['class' => 'btn btn-primary']) !!}
-                {!! link_to_route('shipments.immediate.create', 'Immediate', ['shipment' => $shipment], ['class' => 'btn btn-primary']) !!}
-            </div>
-        @endif
+        <section class="shipment-edit">
+            <h2>Details</h2>
+            <section>
+                <h3>Strecke</h3>
+                <h4>Abholung</h4>
+                @if($shipment->origin_id)
+                    @include('shipments.edit.origin.show', ['origin' => $shipment->origin])
+                    {!! link_to_route('shipments.origin.index', 'Edit', ['shipment' => $shipment], ['class' => 'btn-block btn btn-warning']) !!}
+                @else
+                    {!! link_to_route('shipments.origin.index', 'Select', ['shipment' => $shipment], ['class' => 'btn-block btn btn-primary']) !!}
+                @endif
+                <h4>Lieferung</h4>
+                @if($shipment->destination_id)
+                    @include('shipments.edit.destination.show', ['destination' => $shipment->destination])
+                    {!! link_to_route('shipments.destination.index', 'Edit', ['shipment' => $shipment], ['class' => 'btn-block btn btn-warning']) !!}
+                @else
+                    {!! link_to_route('shipments.destination.index', 'Select', ['shipment' => $shipment], ['class' => 'btn-block btn btn-primary']) !!}
+                @endif
+            </section>
+            <section>
+                <h3>Größe</h3>
+                @if($shipment->size_id)
+                    <p>{{ $shipment->size->name }} <small>{{ $shipment->size->description }}</small></p>
+                    {!! link_to_route('shipments.size.index', 'Edit', ['shipment' => $shipment], ['class' => 'btn-block btn btn-warning']) !!}
+                @else
+                    {!! link_to_route('shipments.size.index', 'Select', ['shipment' => $shipment], ['class' => 'btn-block btn btn-primary']) !!}
+                @endif
+            </section>
+            <section>
+            <h3>Specs</h3>
+                @if(count($shipment->specs))
+                    @foreach($shipment->specs as $spec)
+                        <span class="label label-info"><strong>{{ $spec->name }}</strong> {{ $spec->description }}</span>
+                    @endforeach
+                    {!! link_to_route('shipments.specs.index', 'Edit', ['shipment' => $shipment], ['class' => 'btn-block btn btn-warning']) !!}
+                @else
+                    {!! link_to_route('shipments.specs.index', 'Select', ['shipment' => $shipment], ['class' => 'btn-block btn btn-primary']) !!}
+                @endif
+            </section>
+        </section>
+        <section class="shipment-edit">
+            <h2>Typ</h2>
+            <section>
+                @if($shipment->typeable_id && $shipment->typeable_type)
+                    @if($shipment->typeable instanceof Wundership\Auction)
+                        @include('shipments.edit.type.auction.show', ['shipment' => $shipment, 'auction' => $shipment->typeable])
+                    @elseif($shipment->typeable instanceof Wundership\Immediate)
+                        @include('shipments.edit.type.immediate.show', ['shipment' => $shipment, 'immediate' => $shipment->typeable])
+                    @endif
+                @else
+                    <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                        {!! link_to_route('shipments.auction.create', 'Auction', ['shipment' => $shipment], ['class' => 'btn btn-primary']) !!}
+                        {!! link_to_route('shipments.immediate.create', 'Immediate', ['shipment' => $shipment], ['class' => 'btn btn-primary']) !!}
+                    </div>
+                @endif
+            </section>
+        </section>
     </div>
 @endsection
