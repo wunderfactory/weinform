cfb79e50babffc51a1011f9e91254040ee124e6a
Moritz Ewert <moritz.ewert@wunderfactory.de>
2015-05-06 19:53:51 +0200

    update immediate creation form

--- resources/views/shipments/edit/type/immediate/create.blade.php
+++ resources/views/shipments/edit/type/immediate/create.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
     <div class="container">
-        <h1>Typ wählen</h1>
+        <h1>Immediate Shipment</h1>
         {!! Form::open(['route' => ['shipments.immediate.store', $shipment]]) !!}
         <div class="form-group">
-            <label for="inputPrice">Preis</label>
+            <label for="inputPrice">Gib ein, was du zu zahlen bereit bist</label>
             <div class="input-group">
                 <div class="input-group-addon"><i class="fa fa-eur"></i></div>
                 {!! Form::text('price', null, ['id' => 'inputPrice', 'class' => 'form-control', '0.00']) !!}
             </div>
         </div>
-        {!! Form::submit('Typ wählen', ['class' => 'btn btn-primary btn-block']) !!}
+        {!! Form::submit('Immediate shipping wählen', ['class' => 'btn btn-primary btn-block']) !!}
         {!! Form::close() !!}
     </div>
 @endsection
