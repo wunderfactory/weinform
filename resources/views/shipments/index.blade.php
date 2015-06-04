258f21deb8a46519766b994d6c4ba6879dae4b40
Patrick Häde <patrick.haede@wunderfactory.de>
2015-06-04 21:50:24 +0200

    hep

--- resources/views/shipments/index.blade.php
+++ resources/views/shipments/index.blade.php
@@ -1,10 +1,14 @@
 @extends('app')
 
 @section('content')
-    <div class="container">
-        @include('shipments.index.menu', ['filter' => $filter, 'sizes' => $sizes, 'specs' => $specs])
-    </div>
-    <div class="container">
-        @include('components.shipment.list.list', ['shipments' => $shipments])
-    </div>
+		
+	    <div class="container">
+	    	<h3>Wähle eine Sendung aus, die Du mitnehmen möchtest.</h3>
+	    	<hr>
+	        @include('shipments.index.menu', ['filter' => $filter, 'sizes' => $sizes, 'specs' => $specs])
+	    </div>
+	    <hr>
+	    <div class="container">
+	        @include('components.shipment.list.list', ['shipments' => $shipments])
+	    </div>
 @endsection
