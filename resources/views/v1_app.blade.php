436b571f2e1ef9397285459035974689f942f13c
Patrick Häde <patrick.haede@wunderfactory.de>
2015-06-08 11:59:25 +0200

    bern homepage final version

--- resources/views/v1_app.blade.php
+++ resources/views/v1_app.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
-        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('/gsd/css/gsdk.css') }}" rel="stylesheet"/>
+        <link href="{{ asset('/css/bernstrap.css') }}" rel="stylesheet"/>
         <link href="{{ asset('/css/bern.css') }}" rel="stylesheet"/>
+        <link href="{{ asset('/css/bernimate.css') }}" rel="stylesheet"/>
         <link href="{{ asset('/css/wundership-icon-font.css') }}" rel="stylesheet"/>
     </head>
     <body>
