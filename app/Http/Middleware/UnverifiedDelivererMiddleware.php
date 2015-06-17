c8ac252a184173150c9c4a6b7c022686be36c1ce
Moritz Ewert <moritz.ewert@wunderfactory.de>
2015-06-17 08:54:43 +0200

    create opposite middleware

---
+++ app/Http/Middleware/UnverifiedDelivererMiddleware.php
@@ -0,0 +1,27 @@
+<?php namespace Wundership\Http\Middleware;
+
+use Closure;
+
+class UnverifiedDelivererMiddleware {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		$user = $request->user();
+		if($user->verification && $user->verification->verified)
+		{
+			return $next($request);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+}
