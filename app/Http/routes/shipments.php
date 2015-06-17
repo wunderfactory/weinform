c822ce18c4a9665c5fd832c1563e5aaeb4e3592a
Moritz Ewert <moritz.ewert@wunderfactory.de>
2015-06-17 08:55:20 +0200

    applied middleware

--- app/Http/routes/shipments.php
+++ app/Http/routes/shipments.php
@@ -11,7 +11,7 @@ Route::resource('shipments.size',                       'ShipmentSizeController'
 Route::post('shipments/{shipment}/size/select',         ['as' => 'shipments.size.select', 'uses' => 'ShipmentSizeController@select']);
 Route::resource('shipments.specs',                      'ShipmentSpecController');
 Route::post('shipments/{shipment}/specs/select',        ['as' => 'shipments.specs.select', 'uses' => 'ShipmentSpecController@select']);
-Route::get('shipments/{shipment}/immediate/book',         ['as' => 'shipments.immediate.book', 'uses' => 'ShipmentImmediateController@book']);
+Route::get('shipments/{shipment}/immediate/book',         ['midleware' => ['auth', 'driver', 'driver.unverified', 'driver.goodRating'],'as' => 'shipments.immediate.book', 'uses' => 'ShipmentImmediateController@book']);
 Route::post('shipments/{shipment}/immediate/book',         ['as' => 'shipments.immediate.dobook', 'uses' => 'ShipmentImmediateController@doBook']);
 Route::resource('shipments.auction',                    'ShipmentAuctionController');
 Route::resource('shipments.immediate',                  'ShipmentImmediateController');
