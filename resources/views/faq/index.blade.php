016b54fbbec4e32e8e2d418f4483ec0c0872e768
Patrick Häde <patrick.haede@wunderfactory.de>
2015-06-19 21:00:36 +0200

    faqs design

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -1,10 +1,42 @@
 @extends('app')
+
 @section('content')
 
-    <form method="GET" action="{{ url("faq") }}">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input name="search">
-        <button type="submit"> Suchen</button>
-    </form>
-{{ $questions }}
+<div class="static_area">
+
+	<div id="faq" class="container">
+
+		<div class="question_container">
+			<h3>Hallo! Wie können wir Dir helfen? Gib Deine Frage einfach unten ein und klick auf suchen...</h3>
+			<div clas="row">
+				<div class="col-md-6">
+					<form method="GET" action="{{ url("faq") }}">
+			        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+			        <input class="form-control" name="search">
+			        </form>
+		    	</div>
+		    	<div class="col-md-6">
+		    		<button class="form-control btn btn-primary" type="submit">Suchen</button>
+		    		
+		    	</div>
+	    	</div>
+	    </div>
+    	<div class="row questions_list">
+			@foreach($questions as $question)
+
+				<div class="warningbox">
+			        <div class="header">
+			            <div><p><strong>{{$question->question}}</strong></p></div>
+			        </div>
+			        <div class="content">
+			            <p>{{ $question->answer }}</p>
+			        </div>
+			    </div>
+
+			@endforeach
+    	</div>
+	</div>
+
+</div>
+
 @endsection
--- resources/views/shipments/index.blade.php
+++ resources/views/shipments/index.blade.php
@@ -1,14 +1,26 @@
 @extends('app')
 
 @section('content')
-		
-	    <div class="container">
-	    	<h3>Wähle eine Sendung aus, die Du mitnehmen möchtest.</h3>
+
+<div class="product_area">
+    <div id="shipments" class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <h4>Wähle eine Sendung aus, die Du mitnehmen möchtest.</h4>
 	    	<hr>
-	        @include('shipments.index.menu', ['filter' => $filter, 'sizes' => $sizes, 'specs' => $specs])
+	        	@include('shipments.index.menu', ['filter' => $filter, 'sizes' => $sizes, 'specs' => $specs])
+		    </div>
+		</div><!-- /row -->
+		<div class="row list_container">
+			<div class="col-md-3">
+                @include('notybox.sidebar_helpbox')
+                @include('notybox.sidebar_faqbox')
+            </div>
+            <div class="col-md-8">
+                @include('components.shipment.list.list', ['shipments' => $shipments])
+          </div><!-- /col-md-8 -->
 	    </div>
-	    <hr>
-	    <div class="container">
-	        @include('components.shipment.list.list', ['shipments' => $shipments])
-	    </div>
+    </div><!-- /container -->
+</div>
+
 @endsection
